75a7d8c78e0dbee34f62798ac1acb55e80f1e1d6
Julian Velasco <juliancamilovelasco@gmail.com>
2025-11-02 22:57:19 -0500

    Deletes test files deprecated

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: true, // ✅ NO abre el navegador
-    screenshot: 'off',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
