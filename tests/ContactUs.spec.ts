ca092ebf6b18049b89d48018363dca783d1f86a8
Julian Velasco <juliancamilovelasco@gmail.com>
2025-11-02 22:41:01 -0500

    Delete test depreceted

--- tests/ContactUs.spec.ts
+++ tests/ContactUs.spec.ts
@@ -2,15 +2,6 @@ import { test, expect } from '@playwright/test';
 import { HomePage } from './PageObjects/HomePage';
 import { ContactUsPage } from './PageObjects/ContactUs';
 
-test('Contact Us link', async ({ page }) => {
-    const home = new HomePage(page);
-    await home.openHomePage();// Usa path relativo ya que baseURL está configurado
-    await home.clickContactUsLink();
-    await page.locator("(//div[contains(text(),'General inquiry')])[1]").click();
-    //expect(page.locator("(//fieldset[@class='form-columns-2'])[1]/descendant::input[@name='firstname']")).toBeVisible();
-
-});
-
 test('Services', async ({ page }) => {
     const home = new HomePage(page);
     const contactUs = new ContactUsPage(page);
